bfae1aef2933c418f256fa8aec5de685daff1ade
duguqi7 <39698660+duguqi7@users.noreply.github.com>
2022-11-08 10:40:46 +0800

    Add files via upload
    upload main.app for the first time

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <unistd.h>
+#include "netbase.h"
+#include "rpg_battle.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Welcom to my game!" << endl;
+    cout << "Game server initing..." << endl;
+    sleep(3);
+    cout << "OK! ready for 2 players to join." << endl;
+
+
+
+    while(1)
+    {
+        sleep(1);
+    }
+    return 0;
+}
