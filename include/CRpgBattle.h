7960b47a7c28d738726173052d4b2998750f786b
duguqi7 <39698660+duguqi7@users.noreply.github.com>
2022-11-14 20:38:21 +0800

    [CHG] test commit by VS code.

--- include/CRpgBattle.h
+++ include/CRpgBattle.h
@@ -1,14 +1,31 @@
 #ifndef CRPGBATTLEH
 #define CRPGBATTLEH
 
+const int c_iClientMode = 1;
+const int c_iServerMode = 2;
+const int c_iUniversalMode = 3;
+
+const int c_iGameStepAction = 1;
+const int c_iGameStepStandby = 2;
 
 class CRpgBattle
 {
     public:
         CRpgBattle();
         virtual ~CRpgBattle();
+        //common functions
+        void SetGamePlayerNum(int players);
+        int  GetGamePlayerNum(void);
+
+        //server functions
+        int CreateServer(void);
+
+        //client functions
+        int CreateClient(void);
+
     protected:
     private:
+        int iWorkMode;
 };
 
 #endif // RPG_BATTLE_H
